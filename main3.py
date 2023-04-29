d2fd5298b527aa1648a22d55192976e89ee4086e
selfishark <toowel20010@yahoo.fr>
2023-04-29 20:39:11 +0100

    update of the code with comments

--- main3.py
+++ main3.py
@@ -1,11 +1,16 @@
 import openai
-import asyncio
-import re
-import whisper
+import asyncio      #take the user input with reverse Engineered EdgeGPT
+import whisper      #transform voice input to text works with ffmpeg library
+import pyaudio
 import boto3
+import os
 import pydub
+import ffmpeg       #to get audio input
 from pydub import playback
-import speech_recognition as sr
+from pydub import AudioSegment
+import speech_recognition as sr     #record microphone input
+import json
+#import EdgeGPT
 from EdgeGPT import Chatbot, ConversationStyle
 
 # Initialize the OpenAI API
@@ -13,39 +18,39 @@ openai.api_key = "APIKEY"
 
 # Create a recognizer object and wake word variables
 recognizer = sr.Recognizer()
-BING_WAKE_WORD = "bing"
-GPT_WAKE_WORD = "boom"  #ChatGPT
+BING_WAKE_WORD = "bing"     #change here, the wake word that activate the microphone 
+GPT_WAKE_WORD = "test"
 
-def get_wake_word(phrase):
+def get_wake_word(phrase):      #check if wake is within voice input
     if BING_WAKE_WORD in phrase.lower():
         return BING_WAKE_WORD
     elif GPT_WAKE_WORD in phrase.lower():
         return GPT_WAKE_WORD
     else:
         return None
-    
-def synthesize_speech(text, output_filename):
-    polly = boto3.client('polly', region_name='us-west-2')
+
+def synthesize_speech(text, output_filename):       #AWS text to speech file
+    polly = boto3.client('polly', region_name='eu-west-2')
     response = polly.synthesize_speech(
         Text=text,
         OutputFormat='mp3',
-        VoiceId='Salli',
+        VoiceId='Brian',
         Engine='neural'
     )
 
     with open(output_filename, 'wb') as f:
         f.write(response['AudioStream'].read())
 
-def play_audio(file):
-    sound = pydub.AudioSegment.from_file(file, format="mp3")
-    playback.play(sound)
+#def play_audio(file):       #speechfile from Polly AWS
+ #   sound = pydub.AudioSegment.from_file(file, format="mp3")
+  #  playback.play(sound)
 
 async def main():
     while True:
-
+        #LISTENING TO MICROPHONE AND CONVERTING AUDIO TO TEXT
         with sr.Microphone() as source:
             recognizer.adjust_for_ambient_noise(source)
-            print(f"Waiting for wake words 'ok bing' or 'ok chat'...")
+            print(f"Waiting for wake words 'bing' or 'gpt'...")
             while True:
                 audio = recognizer.listen(source)
                 try:
@@ -56,7 +61,7 @@ async def main():
                     result = model.transcribe("audio.wav", fp16=False)
                     phrase = result["text"]
                     print(f"You said: {phrase}")
-
+                
                     wake_word = get_wake_word(phrase)
                     if wake_word is not None:
                         break
@@ -66,16 +71,16 @@ async def main():
                     print("Error transcribing audio: {0}".format(e))
                     continue
 
-            print("Speak a prompt...")
-            synthesize_speech('Hello Mr Shark, what can I help you with?', 'response.mp3')
-            play_audio('response.mp3')
+            print("What can I help you with? Selfishark...")      # call the synthesise voice to speak and request the user prompt when it hear the wake world
+            synthesize_speech('What can I help you with, Mr Shark?', 'response.mp3')  # verbally request user prompt after hearing wake world
+            play_audio('response.mp3')  # response after wake world
             audio = recognizer.listen(source)
 
             try:
                 with open("audio_prompt.wav", "wb") as f:
                     f.write(audio.get_wav_data())
                 model = whisper.load_model("base")
-                result = model.transcribe("audio_prompt.wav", fp16=False)
+                result = model.transcribe("audio_prompt.wav", fp16=False)   ##HERE
                 user_input = result["text"]
                 print(f"You said: {user_input}")
             except Exception as e:
@@ -83,20 +88,19 @@ async def main():
                 continue
 
             if wake_word == BING_WAKE_WORD:
-                bot = Chatbot(cookiePath='cookies.json')
-                response = await bot.ask(prompt=user_input, conversation_style=ConversationStyle.precise)
-
+                with open('./cookies.json', 'r') as f:
+                    cookies = json.load(f)
+                bot = Chatbot(cookies=cookies)
+                response = await bot.ask(prompt=input("What can I help you with, today?"), conversation_style=ConversationStyle.creative)
+        
                 for message in response["item"]["messages"]:
                     if message["author"] == "bot":
                         bot_response = message["text"]
-
-                bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
-                # Select only the bot response from the response dictionary
-                for message in response["item"]["messages"]:
+        
+                 Select only the bot response from the response dictionary
+                for message in response ["item"]["messages"]:
                     if message["author"] == "bot":
                         bot_response = message["text"]
-                # Remove [^#^] citations in response
-                bot_response = re.sub('\[\^\d+\^\]', '', bot_response)
 
             else:
                 # Send prompt to GPT-3.5-turbo API
@@ -117,11 +121,13 @@ async def main():
                 )
 
                 bot_response = response["choices"][0]["message"]["content"]
-                
-        print("Bot's response:", bot_response)
-        synthesize_speech(bot_response, 'response.mp3')
-        play_audio('response.mp3')
-        await bot.close()
+
+            print("Bot's response:", bot_response)
+            synthesize_speech(bot_response, 'response.mp3') #Speak response from wake work ()
+            play_audio('response.mp3')  #play audio response
+            #await bot.close()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
+	
